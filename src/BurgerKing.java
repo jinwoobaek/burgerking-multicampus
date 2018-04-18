0458547c0b3f3bfe8e2ce2dc59601633f15847a3
student <student@M15011>
2018-04-18 11:19:20 +0900

    이제 시작이야

--- src/BurgerKing.java
+++ src/BurgerKing.java
@@ -11,92 +11,67 @@ public class BurgerKing implements ActionListener {
 	JFrame f;
 	public static CardLayout card;
 	JPanel btnPanel, cardPanel, testPanel;
-	JButton btnOrderView, btnStockView, btnSalesView, btnEmployeeView, btn;
+	JButton btnOrderView, btnStockView, btnSalesView, btnEmployeeView;
 
-	
-	
-	
 	public BurgerKing() {
-		
 		addLayout();
 		eventProc();
-
-		
 	}
 
 	public static void main(String[] args) {
 		new BurgerKing();
 	}
-	
-	
-	void eventProc(){
+
+	void eventProc() {
 		btnOrderView.addActionListener(this);
 		btnStockView.addActionListener(this);
 		btnSalesView.addActionListener(this);
 		btnSalesView.addActionListener(this);
-		btn.addActionListener(this);
 	}
-	
-	void addLayout(){
+
+	void addLayout() {
 		f = new JFrame(); // 메인 프레임
-		card = new CardLayout(); 
+		card = new CardLayout();
 		btnPanel = new JPanel(); // 버튼패널
-		btnOrderView = new JButton("주문"); 
-		btnStockView = new JButton("재고"); 
+		btnOrderView = new JButton("주문");
+		btnStockView = new JButton("재고");
 		btnSalesView = new JButton("매출");
 		btnEmployeeView = new JButton("직원");
-		
-		
-		btnPanel.setLayout(new GridLayout(4,1,5,5));
+
+		btnPanel.setLayout(new GridLayout(4, 1, 5, 5));
 		btnPanel.add(btnOrderView);
 		btnPanel.add(btnStockView);
 		btnPanel.add(btnSalesView);
 		btnPanel.add(btnEmployeeView);
-		btnPanel.setSize(20,15);
-		
-		
-		testPanel = new JPanel();
-		testPanel.setLayout(new GridLayout(2, 2));
-		testPanel.setBackground( Color.red );
-		btn = new JButton("BACK");
-		testPanel.add(btn);
-		
-		
-		
-		cardPanel= new JPanel(card);
-		cardPanel.add(btnPanel,"btnPanel");
-		cardPanel.add(testPanel,"pan");
-		
-	
-		
+		btnPanel.setSize(20, 15);
+
+		cardPanel = new JPanel(card);
+		cardPanel.add(btnPanel, "btnPanel");
+
 		f.add(cardPanel);
 
 		f.setSize(800, 600);
 		f.setVisible(true);
 
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 	}
-	
-	
-	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		Object evt = e.getSource();
 
 		if (evt == btnOrderView) {
 			card.show(cardPanel, "pan");
+		} else if (evt == btnStockView) {
+
+		} else if (evt == btnSalesView) {
+
+		} else if (evt == btnEmployeeView) {
 
 		}
-		
-		if(evt==btn){
-			card.first(cardPanel);
-		}
 
-		
 	}
 
 }
-

e8e50de299cbd65ccdb6ef6bc24ee9a58c550c8e
student <student@M15011>
2018-04-18 14:54:31 +0900

    밀었다

--- src/view/EmployeeView.java
+++ src/view/EmployeeView.java
@@ -4,18 +4,22 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.border.TitledBorder;
+import javax.swing.table.AbstractTableModel;
 
 public class EmployeeView extends JPanel implements ActionListener {
-	JTextField tf_EmpNo, tf_EmpName, tf_IdNo, tf_Gender, tf_Addr, tf_Job, tf_Salary, tf_Home, tf_EmpNoSearch;
+	JTextField tf_EmpNo, tf_EmpName, tf_IdNo, tf_Gender, tf_Addr, tf_Job, tf_Salary, tf_Home;
+	JButton btn_Regist, btn_Modify, btn_Delete, btn_Home;
 
-	JButton btn_Regist, btn_Modify, btn_Delete, btn_Home, btn_EmpNoSearch, btn_EmpNameSearch;
+	JComboBox com_EmpSearch;
+	JTextField tf_EmpSearch;
 
 	JTable tableEmployee;
 
@@ -33,16 +37,16 @@ public class EmployeeView extends JPanel implements ActionListener {
 		tf_Job = new JTextField();
 		tf_Salary = new JTextField();
 		tf_Home = new JTextField();
-		tf_EmpNoSearch = new JTextField();
 
-		btn_Regist = new JButton();
-		btn_Modify = new JButton();
-		btn_Delete = new JButton();
-		btn_Home = new JButton();
-		btn_EmpNoSearch = new JButton();
-		btn_EmpNameSearch = new JButton();
-		
-		setLayout(new GridLayout(1,2));
+		btn_Regist = new JButton("직원등록");
+		btn_Modify = new JButton("정보변경");
+		btn_Delete = new JButton("해고");
+		btn_Home = new JButton("Home");
+
+		String[] cbEmpSearch = { "이름", "사번" };
+		com_EmpSearch = new JComboBox(cbEmpSearch);
+		tf_EmpSearch = new JTextField();
+
 		// ************화면구성************
 		// 왼쪽영역
 		JPanel p_west = new JPanel();
@@ -57,11 +61,11 @@ public class EmployeeView extends JPanel implements ActionListener {
 		p_west_center.add(tf_EmpName);
 		p_west_center.add(new JLabel("주민등록번호"));
 		p_west_center.add(tf_IdNo);
-		p_west_center.add(new JLabel("성별")); // 콤보박스로 만들기
+		p_west_center.add(new JLabel("성별")); // **콤보박스로 만들기
 		p_west_center.add(tf_Gender);
 		p_west_center.add(new JLabel("주소"));
 		p_west_center.add(tf_Addr);
-		p_west_center.add(new JLabel("계급"));
+		p_west_center.add(new JLabel("계급")); // **콤보박스로 만들기
 		p_west_center.add(tf_Job);
 		p_west_center.add(new JLabel("연봉"));
 		p_west_center.add(tf_Salary);
@@ -73,26 +77,23 @@ public class EmployeeView extends JPanel implements ActionListener {
 		p_west_south.add(btn_Modify);
 		p_west_south.add(btn_Delete);
 
+		// 왼쪽 위아래 합체
 		p_west.add(p_west_center, BorderLayout.CENTER);
 		p_west.add(p_west_south, BorderLayout.SOUTH);
 
-		//JPanel p_east = new JPanel();
-		
-		//p_east.add(p_west_center);
+		// 오른쪽 영역
+		JPanel p_east = new JPanel();
+		p_east.setLayout(new BorderLayout());
 
-		add(p_west, BorderLayout.WEST);
-		//add(p_east, BorderLayout.EAST);
-		// // 왼쪽 가운데의 가운데
-		// JPanel p_west_center_center = new JPanel();
-		// p_west_center_center.setLayout(new BorderLayout());
-		// // BorderLayout은 영역 설정도 해야함
-		// p_west_center_center.add(new JLabel("설명"), BorderLayout.WEST);
-		// p_west_center_center.add(taVideoContent, BorderLayout.CENTER);
+		// 오른쪽 위
+		JPanel p_east_north = new JPanel();
+		// p_east_north.setLayout(mgr);
 
-		// 왼쪽 화면에 붙이기
-		// p_west_center.add(p_west_center_north, BorderLayout.NORTH);
-		// p_west_center.add(p_west_center_center, BorderLayout.CENTER);
-		// p_west_center.setBorder(new TitledBorder("회원 정보입력"));
+		// 전체 화면에 왼쪽 오른쪽 붙이기
+		setLayout(new GridLayout(1, 2));
+
+		add(p_west);
+		// add(p_east);
 
 	}
 
@@ -106,4 +107,34 @@ public class EmployeeView extends JPanel implements ActionListener {
 
 	}
 
+}
+
+// 화면에 테이블 붙이는 메소드
+class EmployeeTableModel extends AbstractTableModel {
+
+	ArrayList data = new ArrayList();
+	String[] columnNames = { "사원번호", "이름", "주민등록번호", "감독", "배우" };
+
+	// =============================================================
+	// 1. 기본적인 TabelModel 만들기
+	// 아래 세 함수는 TabelModel 인터페이스의 추상함수인데
+	// AbstractTabelModel에서 구현되지 않았기에...
+	// 반드시 사용자 구현 필수!!!!
+
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	public int getRowCount() {
+		return data.size();
+	}
+
+	public Object getValueAt(int row, int col) {
+		ArrayList temp = (ArrayList) data.get(row);
+		return temp.get(col);
+	}
+
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
 }
